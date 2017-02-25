33060ba297314f314c5e812eacaf1d14c85fc68e
Pierre Morel <pier.morel@gmail.com>
2017-02-25 18:19:44 +0100

    Added the possibility to provide a custom function to stat_summary()

--- @gramm/stat_summary.m
+++ @gramm/stat_summary.m
@@ -159,7 +159,7 @@ if iscell(draw_data.x) || iscell(draw_data.y) %If input was provided as cell/mat
         warning('bin_in in stat_summary() not supported for Matrix/Cell X/Y inputs');
     end
     
-    if strfind(params.type,'fit')
+    if ischar(params.type) && ~isempty(strfind(params.type,'fit'))
         %If we have a params.type using distributions fits we
         %can't vectorize the call to computeci so we do it in a for
         %loop
@@ -343,6 +343,19 @@ end
 if nsamp<2
     disp('Less than 2 samples for CI computation...Skipping')
     yci=repmat([NaN NaN],size(y,2));
+    return;
+end
+
+if isa(type,'function_handle')
+    try
+        temp=type(y);
+        ymean=temp(1,:);
+        yci=temp(2:3,:);
+    catch ME
+        disp(['Error in custom summary computation: ' ME.message ' ...skipping']);
+        yci=repmat([NaN NaN],size(y,2));
+        ymean=nan(size(y,2),1);
+    end
     return;
 end
 
