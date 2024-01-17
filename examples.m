084ac50ea656b665df0ead14b5e3e8f9d9b2302c
Pierre Morel <pier.morel@gmail.com>
2024-01-17 11:37:05 +0100

    Added example on how to fake a broken axis using facet_grid()
    Fixes #128

--- examples.m
+++ examples.m
@@ -1011,6 +1011,27 @@ g.axe_property('TickDir','out','XGrid','on','Ygrid','on','GridColor',[0.5 0.5 0.
 figure('Position',[100 100 700 350]);
 g.draw();
 
+%% Create a broken axis
+
+x=[randn(1,50)*2 randn(1,50)*8+70];
+y=-x;
+g(1,1)=gramm('x',x,'y',y);
+g(1,1).geom_point();
+g(1,1).set_title('Without broken axis');
+
+
+g(2,1)=gramm('x',x,'y',y)
+g(2,1).facet_grid([],x>20,"scale","free_x","space","free_x","column_labels",false)
+g(2,1).geom_point()
+g(2,1).set_title('With broken axis');
+
+g.draw()
+g(2,1).facet_axes_handles(2).YAxis.Visible='off';
+g(2,1).facet_axes_handles(1).XLim=[-5 6];
+g(2,1).facet_axes_handles(1).XTick=-5:5:5;
+g(2,1).facet_axes_handles(2).XLim=[50 90];
+g(2,1).facet_axes_handles(2).XTick=50:5:90;
+
 %% Customizing color maps with set_color_options()
 % With the method set_color_options(), automatic color generation for
 % color and lightness groups can be tweaked
