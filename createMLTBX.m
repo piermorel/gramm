f832e91839d61c2d784197bb941853c39e99570a
Bensingh Pancras <bpancras@mathworks.com>
2024-03-28 12:26:57 +0530

    Updated the help text, prints a msg on completion and prefixed the mltbx filename with the toolbox name

--- createMLTBX.m
+++ createMLTBX.m
@@ -1,22 +1,25 @@
 function createMLTBX(prjFile, toolboxVersion)
-% Package the toolbox as MLTBX file.
-% createMLTBX(prjFile, toolboxVersion) builds the MLTBX file and saves
-% it in the release folder. Input prjFile is the name of the toolbox
-% packaging file and toolboxVersion is a string of the form Major.Minor.Bug.Build.
+% Package the toolbox as MLTBX.
+%   createMLTBX(prjFile, toolboxVersion) builds the MLTBX file and saves it
+%   in the working directory. The prjFile is the path to the toolbox packaging
+%   PRJ file and toolboxVersion is a string of the form Major.Minor.Bug.Build.
+%
+%   Example
+%       createMLTBX("gramm.prj", "1.0.1.3")
 
 if ~isfile(prjFile)
-    error("Unable to find " + "'" + prjFile+ "'");
+    error("Unable to find \'%s\'.", prjFile);
 end
 
 packagingData = matlab.addons.toolbox.ToolboxOptions(prjFile);
 
 % Update the version number
 packagingData.ToolboxVersion = toolboxVersion;
-
-% packagingData.OutputFile = fullfile("release", "fsda.mltbx");
-packagingData.OutputFile = strcat(toolboxVersion, ".mltbx");
+outputFileName = packagingData.ToolboxName + "_" + toolboxVersion + ".mltbx";
+packagingData.OutputFile =outputFileName;
 
 % Create toolbox MLTBX
 matlab.addons.toolbox.packageToolbox(packagingData);
 
+fprintf("Created %s.\n", outputFileName);
 end
