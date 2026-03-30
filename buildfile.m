decfb130ce087c2d6585699cbc582f61febb0ac7
Shivam Lahoti <slahoti@mathworks.com>
2026-03-30 15:27:22 +0530

    Using the custom build task for ExampleDrivenTester

--- buildfile.m
+++ buildfile.m
@@ -7,14 +7,10 @@ plan = buildplan(localfunctions);
 plan("clean") = CleanTask;
 plan("check") = CodeIssuesTask(Results="issues.mat");
 
-plan.DefaultTasks = "check";
-end
-
-function runExamplesTask(context)
-% Run examples as tests
 reportFormat = matlab.unittest.plugins.codecoverage.CoverageReport('coverage-report');
 covPlugin = matlab.unittest.plugins.CodeCoveragePlugin.forFolder("gramm","Producing",  reportFormat);
-etObj = examplesTester("gramm/examples", CodeCoveragePlugin = covPlugin);
-etObj.executeTests();
+plan("runExample") = ExampleDrivenTesterTask("gramm/examples", CodeCoveragePlugin = covPlugin);
+
+plan.DefaultTasks = "check";
 end
 
